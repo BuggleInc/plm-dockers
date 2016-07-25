4310d97c73cab3f78f40c4f84964a94d564855fd
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2016-07-25 15:47:20 +0200

    Add support for options to script

--- web-plm/make.sh
+++ web-plm/make.sh
@@ -1,5 +1,56 @@
-docker build --build-arg "BRANCH=rebase-refactor" --build-arg "REPOSITORY=github.com/BuggleInc/webplm.git" -t play-webplm github.com/BuggleInc/plm-dockers.git#update:play
+#!/usr/bin/env bash
 
-docker run -v ~/.ivy2:/root/.ivy2 -v `pwd`/target:/app/target play-webplm stage
+# -b <branch name>, --branch=<branch name> : the branch of the repository to checkout
+# -n <image name>, --name=<image name> : the name of the resulting docker image
+# -v <version>, --version=<version> : the version of the resulting docker image
 
-docker build -t webplm:2.0-rc2 .
+# read the options
+TEMP=`getopt -o hb:n:v: --long help,branch:,name:,version: -n 'make.sh' -- "$@"`
+
+if [ $? != 0 ] ; then
+    echo "Terminating..."
+    exit 1
+fi
+
+eval set -- "$TEMP"
+
+ARG_BRANCH="master"
+ARG_NAME="webplm"
+ARG_VERSION=""
+
+usage() {
+    echo "$0 [-h | --help] [-b | --branch <branch name>] [-n | --name <docker image name>] [-v | --version <version name>]"
+}
+
+while true ; do
+    case "$1" in
+        -h|--help)
+            usage
+            exit 0 ;;
+        -b|--branch)
+            case "$2" in
+                *) ARG_BRANCH=$2 ; shift 2 ;;
+            esac ;;
+        -n|--name)
+            case "$2" in
+                *) ARG_NAME=$2 ; shift 2 ;;
+            esac ;;
+        -v|--version)
+            case "$2" in
+                *) ARG_VERSION=":$2" ; shift 2 ;;
+            esac ;;
+        --) shift ; break ;;
+        *) echo "Internal error!" ; exit 1 ;;
+    esac
+done
+
+docker build --build-arg "BRANCH=$ARG_BRANCH" \
+             --build-arg "REPOSITORY=github.com/BuggleInc/webplm.git" \
+             -t play-webplm \
+             github.com/BuggleInc/plm-dockers.git#update:play
+
+docker run -v ~/.ivy2:/root/.ivy2 \
+           -v `pwd`/target:/app/target \
+           play-webplm stage
+
+docker build -t "$ARG_NAME$ARG_VERSION" .

a82e43d37f34f861a1e9c1ba4e6d10c3e241f991
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2016-07-27 14:47:30 +0200

    Update script to build PLM-judge

--- dockerfile/judge-plm/make.sh
+++ dockerfile/judge-plm/make.sh
@@ -5,15 +5,25 @@
 # --no-cache : flag to build the docker image to generate the binaries using the option --no-cache
 # -r | --repository <repository name> : the git repository to clone
 # -b | --branch <branch name> : the branch of the repository to checkout
+# --bin <path to binaries> : the path to the binaries of webPLM if you already have them
 # -n | --name <docker image name> : the name of the resulting docker image
 # -v | --version <docker image version> : the version of the resulting docker image
 
-# read the options
-TEMP=`getopt -o hcr:b:n:v: --long help,clean,no-cache,repository:branch:,name:,version: -n 'make.sh' -- "$@"`
 
-if [ $? != 0 ] ; then
+usage() {
+    echo "$0 [-h | --help] [-c | --clean] [--no-cache] [-r | --repository <repository name>] [-b | --branch <branch name>] [--bin <path to binaries>] [-n | --name <docker image name>] [-v | --version <version name>]"
+}
+
+terminating() {
     echo "Terminating..."
     exit 1
+
+}
+# read the options
+TEMP=`getopt -o hcr:b:n:v: --long help,clean,no-cache,repository:branch:,bin:,name:,version: -n 'make.sh' -- "$@"`
+
+if [ $? != 0 ] ; then
+    terminating
 fi
 
 eval set -- "$TEMP"
@@ -24,13 +34,10 @@ ARG_NAME="judge"
 ARG_VERSION=""
 ARG_NO_CACHE=false
 ARG_CLEAN=false
+ARG_BIN=""
 
 # One-liner to get the full directory name of the script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-usage() {
-    echo "$0 [-h | --help] [-c | --clean] [--no-cache] [-r | --repository <repository name>] [-b | --branch <branch name>] [-n | --name <docker image name>] [-v | --version <version name>]"
-}
 
 while true ; do
     case "$1" in
@@ -48,6 +55,10 @@ while true ; do
         -b|--branch)
             case "$2" in
                 *) ARG_BRANCH=$2 ; shift 2 ;;
+            esac ;;
+        --bin)
+            case "$2" in
+                *) ARG_BIN=$2 ; shift 2 ;;
             esac ;;
         -n|--name)
             case "$2" in
@@ -68,27 +79,51 @@ else
     DOCKER_IMAGE_FULLNAME="$ARG_NAME"
 fi
 
-# TODO: Factorize this code
-if [ "$ARG_NO_CACHE" = true ]; then
-    docker build --build-arg "REPOSITORY=$ARG_REPOSITORY" \
-                 --build-arg "BRANCH=$ARG_BRANCH" \
-                 --no-cache \
-                 -t play-judge \
-                 github.com/BuggleInc/plm-dockers.git#update:play
-else
-    docker build --build-arg "REPOSITORY=$ARG_REPOSITORY" \
-                 --build-arg "BRANCH=$ARG_BRANCH" \
-                 -t play-judge \
-                 github.com/BuggleInc/plm-dockers.git#update:play
-fi
+if [ -n "$ARG_BIN" ]; then
 
-docker run -v ~/.ivy2:/root/.ivy2 \
-           -v "$DIR/target:/app/target" \
-           play-judge assembly
+    # We already have the binaries
+    cp -r "$ARG_BIN" "$DIR/"
+
+    if [ $? -eq 1 ]; then
+        echo "An error occurred while copying the binaries."
+        terminating
+    fi
+
+else
+
+    echo "Generating docker image to build the project available here: $ARG_REPOSITORY"
+    echo "The current branch is $ARG_BRANCH"
+
+    docker build --build-arg "REPOSITORY=$ARG_REPOSITORY" \
+                 --build-arg "BRANCH=$ARG_BRANCH" \
+                 "--no-cache=$ARG_NO_CACHE" \
+                 -t play-judge \
+                 github.com/BuggleInc/plm-dockers.git#update:dockerfile/play
+
+    if [ $? -eq 1 ]; then
+        echo "An error occurred while generating the docker image to build the project."
+        terminating
+    fi
+
+    docker run -v ~/.ivy2:/root/.ivy2 \
+               -v "$DIR/target:/app/target" \
+               --rm play-judge assembly
+
+    if [ $? -eq 1 ]; then
+        echo "An error occurred while building the project."
+        terminating
+    fi
+
+fi
 
 echo "Binaries of PLM-judge are available in $DIR/target/"
 
 docker build -t "$DOCKER_IMAGE_FULLNAME" .
+
+if [ $? -eq 1 ]; then
+    echo "An error occurred while generating the docker image to run the project."
+    terminating
+fi
 
 if [ "$ARG_CLEAN" = true ]; then
     echo "Deleting directory $DIR/target/..."
